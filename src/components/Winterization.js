474188d8773936a56a88ab975f3674e209bcca92
wwstrothe <williamstrothe@gmail.com>
2022-11-17 23:04:41 -0700

    Winterization.js

--- src/components/Winterization.js
+++ src/components/Winterization.js
@@ -1,27 +1,73 @@
 import React, { useState } from 'react';
-import { Collapse, Container } from 'react-bootstrap';
+import { Card, Collapse, Table } from 'react-bootstrap';
 
 function Winterization() {
   const [open, setOpen] = useState(false);
 
   return (
-    <Container>
-      <div
-        variant="outline-secondary"
-        onClick={() => setOpen(!open)}
-        aria-controls="example-collapse-text"
-        aria-expanded={open}
-      >
-        <h2>Winterization</h2>
-      </div>
-      <Collapse in={open}>
-        <div id="example-collapse-text">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
-          terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
-          labore wes anderson cred nesciunt sapiente ea proident.
-        </div>
-      </Collapse>
-    </Container>
+    <Card>
+      <Card.Body>
+        <Card.Title
+          onClick={() => setOpen(!open)}
+          aria-controls="example-collapse-text"
+          aria-expanded={open}
+        >
+          Winterization
+        </Card.Title>
+
+        <Collapse in={open}>
+          <div>
+            <Table striped bordered>
+              <thead>
+                <tr>
+                  <th>Property Preservation Type</th>
+                  <th>Comments</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Snow Removal </td>
+                  <td>Up to $75 one time only if needed to secure property </td>
+                </tr>
+                <tr>
+                  <td>Winterization/De-winterization </td>
+                  <td>
+                    Up to $200 for first unit and up to $80 per each additional
+                    unit.
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+
+            <Card.Text>
+              Generally, all properties should be winterized between October 1
+              and March 31.
+            </Card.Text>
+            <Card.Text>
+              Winterizations are allowed during any month of the year in the
+              following states:
+            </Card.Text>
+            <Card.Text>
+              AK, CT, IA, IL, IN, MA, ME, MI, MN, MT, ND, NE, NH, NY, OH, PA,
+              RI, SD, VT, WI
+            </Card.Text>
+            <Card.Text>
+              Properties should be winterized only once. The property may be
+              re-winterized only if the initial winterization is deemed to be no
+              longer effective. The Servicer must submit bids to Fannie Mae for
+              winterization prior to the work being done. Winterizations should
+              include shutting off the water source either at the curb or the
+              main interior water supply if it is not possible to shut it off at
+              the curb. Winterization also includes a complete draining of all
+              plumbing and heating systems. For properties where water services
+              and utilities are shared with other units, such as condominiums,
+              the utilities should be maintained. The water service should also
+              be maintained if needed for wet winterizations.
+            </Card.Text>
+          </div>
+        </Collapse>
+      </Card.Body>
+    </Card>
   );
 }
 
